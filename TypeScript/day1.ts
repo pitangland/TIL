1577d9ea32179b6eb1b64263adefb6f605575917
pitang <wpeffect@gmail.com>
2023-01-05 20:56:15 +0900

    0105

--- TypeScript/day1.ts
+++ TypeScript/day1.ts
@@ -1,9 +1,5 @@
-let person = { name: "Wonpyo", age: 22 };
-let { name: string, age: int } = person;
-
-let array = [1, 2, 3, 4];
-let [head, ...rest] = array;
-
-let a = 1,
-  b = 2;
-[a, b] = [b, a];
+function sayHello(person: string) {
+  return "Hello, " + person;
+}
+let user = "Jane User";
+document.body.textContent = sayHello(user);
