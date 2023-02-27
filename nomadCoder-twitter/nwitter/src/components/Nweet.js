67edaace8a053963c0800d0dd624afff72271206
pitang <wpeffect@gmail.com>
2023-02-28 01:46:38 +0900

    23/02/28

--- nomadCoder-twitter/nwitter/src/components/Nweet.js
+++ nomadCoder-twitter/nwitter/src/components/Nweet.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
-import { dbService } from "fBase";
+import { dbService, storageService } from "fBase";
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { deleteObject, ref } from "@firebase/storage";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
 
   const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  const NweetImgRef = ref(storageService, nweetObj.attachmentUrl);
 
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure? you want to delete this nweet?");
     if (ok) {
       await deleteDoc(NweetTextRef);
+      await deleteObject(NweetImgRef);
     }
   };
 
@@ -53,6 +56,14 @@ const Nweet = ({ nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img
+              src={nweetObj.attachmentUrl}
+              widht="50px"
+              height="50px"
+              alt="업로드된 사진"
+            />
+          )}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Nweet</button>
